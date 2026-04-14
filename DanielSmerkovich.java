f93c716dbc824797eabd6a29550cfcdd3af07779
Daniel Smerkovich <danielsmerko13@gmail.com>
2026-04-14 16:04:22 +0300

    I have updated DanielSmerkovich.java code to include a diffrent print

--- DanielSmerkovich.java
+++ DanielSmerkovich.java
@@ -1,7 +1,7 @@
 public class DanielSmerkovich {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Daniel Smerkovich");
+            System.out.println("Daniel Smerkovich is interested in DevOps methedologies..." + i);
         }
     }
 }
